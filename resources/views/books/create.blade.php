cc5ebf8b89d8ee0eb719018f7dbd0d107578f945
Md Nayem Molla <mdnayemmolla077@gmail.com>
2026-04-23 00:03:23 +0100

    Merge branch 'main' of https://github.com/mdnayemmolla077-hue/book-tracker

--- resources/views/books/create.blade.php
+++ resources/views/books/create.blade.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -28,35 +27,4 @@
         </form>
 
     </div>
-=======
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Add Book
-        </h2>
-    </x-slot>
-
-    <div class="py-6 max-w-7xl mx-auto sm:px-6 lg:px-8">
-
-        <form method="POST" action="{{ route('books.store') }}">
-            @csrf
-
-            <div>
-                <label>Title</label>
-                <input type="text" name="title" class="border p-2 w-full">
-            </div>
-
-            <div class="mt-4">
-                <label>Author</label>
-                <input type="text" name="author" class="border p-2 w-full">
-            </div>
-
-            <button class="mt-4 bg-green-500 text-white px-4 py-2 rounded">
-                Save
-            </button>
-
-        </form>
-
-    </div>
->>>>>>> b6ddffdd0510391814095279015b2f8dcc479bb6
 </x-app-layout>
